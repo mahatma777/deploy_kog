6d17215c4474acca828275ac6953f9998a4d7f75
mahatma777 <vicdeblav@gmail.com>
2024-11-08 04:52:12 +0000

    updaten

--- src/feature-module/inventory/WebCam.jsx
+++ src/feature-module/inventory/WebCam.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Webcam from "react-webcam";
 
-const WebcamComponent = () => <Webcam />;
+// const WebcamComponent = () => <Webcam />;
 
 const videoConstraints = {
     width: 300,
