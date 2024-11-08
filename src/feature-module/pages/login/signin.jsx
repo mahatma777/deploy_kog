7d60ac26c5ebce60485063b46bf9dd64dceff4ab
mahatma777 <vicdeblav@gmail.com>
2024-11-08 15:21:42 +0000

    ham

--- src/feature-module/pages/login/signin.jsx
+++ src/feature-module/pages/login/signin.jsx
@@ -28,7 +28,7 @@ const Signin = () => {
     axios.defaults.withCredentials =true;
     const handleSumbit= (event) =>{
     event.preventDefault();
-    axios.post('https://kogiagile.org/kogi_agile/backend/server.js/login', values)
+    axios.post('https://kogiagile.org/kogi_agile/backend/login', values)
     .then(res => {
       if(res.data.Status === "Success"){
       
